24afceb3121dcbb820a52af928854884407fdfb7
amayanm <74217770+amayanm@users.noreply.github.com>
2021-02-07 15:43:12 -0500

    Created quote app

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # dig4639-ex2
+
+“Never let the fear of striking out keep you from playing the game”
+― Babe Ruth
---
+++ index.js
@@ -0,0 +1,11 @@
+let quotes = [
+    `"Never let the fear of striking out keep you from playing the game"
+    ― Babe Ruth`,
+    `"If you focus on what you left behind, you will never see what lies ahead."
+    — Gusteau, Ratatouille`,
+    `"The way to get started is to quit talking and begin doing." 
+    -Walt Disney`,
+]
+
+let index = Math.floor (Math.random() * 3);
+console.log(quotes[index])
